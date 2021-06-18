a75802c078a7faffc8ff282ef3b30ee9eeb5086a
omarqr0 <pleess30@gmail.com>
2021-06-18 17:00:36 +0300

    commit message

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect , url_for, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:509018@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+migrate = Migrate(app,db)
+
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description= db.Column(db.String, nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  error =False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    db.session.rollback()
+    error=True
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if not error:
+    return jsonify(body)
+
+#dd
+@app.route('/')
+def index():
+  return render_template('index.html', data = Todo.query.all())
